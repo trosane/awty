66594d81afd607ac1ac79ad68bf91618f032fab7
TRENT ROSANE <trosane@uw.edu>
2015-05-13 16:33:51 -0700

    first commit

---
+++ app/src/main/java/edu/washington/trosane/arewethereyet/MainActivity.java
@@ -0,0 +1,119 @@
+package edu.washington.trosane.arewethereyet;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.content.Intent;
+
+public class MainActivity extends ActionBarActivity {
+    Button startButton;
+    TextView messageText;
+    TextView phoneText;
+    TextView timeText;
+    static int timeInt;
+    static String toastMessage;
+    EditText editTextMessage, editTextPhone, editTextTime;
+    Intent start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startButton = (Button) findViewById(R.id.startButton);
+        messageText = (TextView) findViewById(R.id.messageBox);
+        phoneText = (TextView) findViewById(R.id.phoneNumberBox);
+        timeText = (TextView) findViewById(R.id.timeBox);
+
+        editTextMessage = (EditText) findViewById(R.id.message);
+        editTextPhone = (EditText) findViewById(R.id.phone);
+        editTextTime = (EditText) findViewById(R.id.time);
+
+        start = new Intent(MainActivity.this, TimeService.class);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                String timeString = editTextTime.getText().toString();
+                toastMessage = editTextPhone.getText().toString();
+                if (startButton.getText().toString().equals("Start")) {
+                    if (validateMessage(editTextMessage.getText().toString()) && validatePhoneNumber
+                            (editTextPhone.getText().toString()) && validateTime(timeString)) {
+                        startButton.setText("Stop");
+                        startService(start);
+                    }
+                } else {
+                    startButton.setText("Start");
+                    stopService(start);
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // returns true if the EditText is not empty, false if it is
+    public boolean validateMessage(String message) {
+        if (editTextMessage.getText().equals("")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // returns true if the phone number is valid, false if not
+    public static boolean validatePhoneNumber(String phoneNumber) {
+        if(phoneNumber.matches("\\d{10}"))
+            return true;
+        else if(phoneNumber.matches("\\d{3}[-\\s]\\d{3}[-\\s]\\d{4}"))
+            return true;
+        else
+            return false;
+    }
+
+    // returns true if the time is greater than zero, false if not
+    public static boolean validateTime(String time) {
+        timeInt = Integer.parseInt(time);
+        if (timeInt > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static int getTime() {
+        return timeInt;
+    }
+
+    public static String getToast() {
+        return toastMessage + ": Are we there yet?";
+    }
+}
---
+++ app/src/main/java/edu/washington/trosane/arewethereyet/TimeService.java
@@ -0,0 +1,55 @@
+package edu.washington.trosane.arewethereyet;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.Handler;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.lang.Runnable;
+import android.widget.Toast;
+import android.content.Context;
+
+public class TimeService extends Service {
+    public static final int INTERVAL = MainActivity.getTime() * 60000;
+
+    private Handler mHandler = new Handler();
+
+    private Timer mTimer = null;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        if(mTimer != null) {
+            mTimer.cancel();
+        } else {
+            mTimer = new Timer();
+        }
+
+        mTimer.scheduleAtFixedRate(new TimeDisplayTimerTask(), 0, INTERVAL);
+    }
+
+    public void onDestroy(){
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
+        mTimer.cancel();
+    }
+
+    class TimeDisplayTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+            mHandler.post(new Runnable() {
+
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(), MainActivity.getToast(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+}
